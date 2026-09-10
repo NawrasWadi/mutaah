8d5e2fd286f5eb1f273be53dc609b09a0ae3e7cd
NawrasWadi <nsmw0393@gmail.com>
2026-09-10 23:17:11 +0300

    feat(auth): wire AdminAccessProvider into root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ProductsProvider } from "@/context/ProductsContext";
 import { UserProfileProvider } from "@/context/UserProfileContext";
 import { NotificationsProvider } from "@/context/NotificationsContext";
 import Providers from "./providers";
+import { AdminAccessProvider } from "@/context/AdminAccessContext";
 
 const cairo = Cairo({
   subsets: ["arabic"],
@@ -40,18 +41,20 @@ export default function RootLayout({
     <html lang="ar" dir="rtl" suppressHydrationWarning>
       <body className={`${cairo.variable} ${plusJakarta.variable} ${materialSymbols.variable} antialiased font-sans bg-slate-50 text-slate-900 flex flex-col min-h-screen`}>
        <Providers>
-         <ProductsProvider>
-          <UserProfileProvider>
-            <NotificationsProvider>
-              <div className="flex-1 flex flex-col">
-                {children}
-              </div>
-              <Footer />
-              <ChatbotWrapper  />
-            </NotificationsProvider>
-          </UserProfileProvider>
-         </ProductsProvider>
-       </Providers>
+  <ProductsProvider>
+    <UserProfileProvider>
+      <AdminAccessProvider>
+        <NotificationsProvider>
+          <div className="flex-1 flex flex-col">
+            {children}
+          </div>
+          <Footer />
+          <ChatbotWrapper />
+        </NotificationsProvider>
+      </AdminAccessProvider>
+    </UserProfileProvider>
+  </ProductsProvider>
+</Providers>
       </body>
     </html>
   );
